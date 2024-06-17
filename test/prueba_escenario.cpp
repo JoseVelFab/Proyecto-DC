b6254b13511a3f11bab995c086dd13fe3071f4bb
Andres Flores Bañuelos <159966414+Andres-Flores-Ba@users.noreply.github.com>
2024-06-16 18:45:26 -0600

    Poner el mapa como fondo

--- test/prueba_escenario.cpp
+++ test/prueba_escenario.cpp
@@ -1,63 +1,34 @@
-#include <Escenario.hpp>
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-#include <random>
-#include <windows.h> // Incluir la biblioteca windows.h
-
-/*int main() {
-    const int filas = 25;
-    const int columnas = 25;
-
-    // Declarar una matriz estática de 25x25
-    int matriz[filas][columnas];
-
-    // Configurar el generador de números aleatorios
-    std::random_device rd;  // Objeto para obtener una semilla
-    std::mt19937 gen(rd()); // Generador de números aleatorios Mersenne Twister
-    std::uniform_int_distribution<> dis(1, 3); // Distribución uniforme entre 1 y 3
-
-    // Llenar la matriz con números aleatorios del 1 al 3
-    for (int i = 0; i < filas; ++i) {
-        for (int j = 0; j < columnas; ++j) {
-            matriz[i][j] = dis(gen);
-        }
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Prueba de Escenario");
+    
+    sf::Texture texture;
+    if (!texture.loadFromFile("assets/Mapa/Mapa.png"))
+    {
+        // Error al cargar la imagen
+        return -1;
     }
-
-    // Agrupar los números 3
-    bool encontrado = false;
-    for (int i = 0; i < filas; ++i) {
-        for (int j = 0; j < columnas; ++j) {
-            if (matriz[i][j] == 3 && !encontrado) {
-                encontrado = true;
-            } else if (matriz[i][j] == 3 && encontrado) {
-                matriz[i][j] = matriz[i][j-1];
-                matriz[i][j-1] = 3;
+    
+    sf::Sprite background(texture);
+    window.setSize(sf::Vector2u(texture.getSize().x, texture.getSize().y));
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
             }
         }
+        
+        window.clear();
+        window.draw(background);
+        window.display();
     }
-
-    // Cambiar el color de la salida de la consola a rojo
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-
-    // Imprimir la matriz
-    for (int i = 0; i < filas; ++i) {
-        for (int j = 0; j < columnas; ++j) {
-            std::cout << matriz[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    // Restaurar el color de la salida de la consola
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-
-    return 0;
-}
-*/
-
-int main(int argc, char const *argv[])
-{
-    sf::RenderWindow window(sf::VideoMode(600, 400), "Mapa");
-    Escenario escenario;
+    
     return 0;
 }
